6c41cb8f39cf7be5ae80b36dbbf3b49489c9002d
milan <meelonkhanal2@gmail,com>
2024-08-14 21:07:35 +0545

    test

--- src/main/java/com/milan/codechangepresentationgenerator/service/impl/PresentationServiceImpl.java
+++ src/main/java/com/milan/codechangepresentationgenerator/service/impl/PresentationServiceImpl.java
@@ -161,15 +161,20 @@ public class PresentationServiceImpl implements PresentationService {
         presentation.setData(pptData);
 
         presentationRepository.save(presentation);
+    }    private void ddd(String presentationId, byte[] pptData) {
+        Presentation presentation = new Presentation();
+        presentation.setId(presentationId);
+        log.info("Presentation id {}", presentationId);
+        presentation.setData(pptData);
+
+        presentationRepository.save(presentation);
     }
 
     @Override
     public byte[] getPresentation(Long id) {
         return new byte[0];
     }
-    public byte[] ss(Long id) {
-        return new byte[0];
-    }
+
 
     @Override
     public void savePresentation(String presentationId) {
